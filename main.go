7ad758fcc498b43603142e4fb86d46aaf18ce1ff
Lewis <ljoncb@gmail.com>
2023-07-25 19:06:15 +0100

    added a repl that can output lexer tokens

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/auenc/mwnci/repl"
+)
 
 func main() {
-	fmt.Println("hello, world")
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("S'mae %s! Mae hi'n iath y rhaglennu Mwnci\n", user.Username)
+	fmt.Printf("Teipio gorchmynion os dych chi eisiau\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
